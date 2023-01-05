14dcea1e3e8114d4beababfb549bf1abc39ac5f8
Naime Medjek <naimemedjek@gmail.com>
2023-01-05 15:38:41 +0100

    added book model and appsettings hidden

---
+++ MyLibrary/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace MyLibrary.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Author { get; set; }
+    public string ReleaseDate { get; set; }
+
+    public Category Category { get; set; }
+
+}
+
+public enum Category
+{
+    Fiction,
+    NonFiction
+}

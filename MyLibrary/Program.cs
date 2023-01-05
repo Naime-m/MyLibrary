1083f413a70a24ba236bbc795a3dac35d1da57ce
Naime Medjek <naimemedjek@gmail.com>
2023-01-05 19:04:16 +0100

    changed default home to books/index

--- MyLibrary/Program.cs
+++ MyLibrary/Program.cs
@@ -26,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Books}/{action=Index}/{id?}");
 
 app.Run();

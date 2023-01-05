2738a1ff4c01062820411040683042a578095ef5
Naime Medjek <naimemedjek@gmail.com>
2023-01-05 16:08:53 +0100

    added empty views and books controller

---
+++ MyLibrary/Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyLibrary.Data;
+
+namespace MyLibrary.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly MyLibraryDbContext _context;
+
+        public BooksController(MyLibraryDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

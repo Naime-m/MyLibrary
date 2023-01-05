21b51a3c81f982533993949cf718b17a7c67fd14
Naime Medjek <naimemedjek@gmail.com>
2023-01-05 17:11:33 +0100

    added create get method

--- MyLibrary/Controllers/BooksController.cs
+++ MyLibrary/Controllers/BooksController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using MyLibrary.Data;
 using MyLibrary.Models;
+using System.Reflection.Metadata.Ecma335;
 
 namespace MyLibrary.Controllers
 {
@@ -21,5 +22,22 @@ namespace MyLibrary.Controllers
                 return View(Books);
         }
         public IEnumerable<Book> Books { get; set; } = Enumerable.Empty<Book>();
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,Author,ReleaseDate")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                 _context.Add(book);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
     }
 }
